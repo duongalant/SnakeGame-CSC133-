dbba9f1a048041dbc9ca15dce20b45b6ae53f49e
Alan Duong <alantduong@csus.edu>
2024-03-18 20:03:04 -0700

    Added a Drawable interface and implemented it inside of Apple and Snake

--- app/src/main/java/com/gamecodeschool/c17snake/Apple.java
+++ app/src/main/java/com/gamecodeschool/c17snake/Apple.java
@@ -8,7 +8,7 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import java.util.Random;
 
-class Apple {
+class Apple implements Drawable {
 
     // The location of the apple on the grid
     // Not in pixels
@@ -54,7 +54,7 @@ class Apple {
     }
 
     // Draw the apple
-    void draw(Canvas canvas, Paint paint){
+    public void draw(Canvas canvas, Paint paint){
         canvas.drawBitmap(mBitmapApple,
                 location.x * mSize, location.y * mSize, paint);
 
--- app/src/main/java/com/gamecodeschool/c17snake/Snake.java
+++ app/src/main/java/com/gamecodeschool/c17snake/Snake.java
@@ -11,7 +11,7 @@ import android.view.MotionEvent;
 
 import java.util.ArrayList;
 
-class Snake {
+class Snake implements Drawable {
 
     // The location in the grid of all the segments
     private ArrayList<Point> segmentLocations;
@@ -205,7 +205,8 @@ class Snake {
         return false;
     }
 
-    void draw(Canvas canvas, Paint paint) {
+    @Override
+    public void draw(Canvas canvas, Paint paint) {
 
         // Don't run this code if ArrayList has nothing in it
         if (!segmentLocations.isEmpty()) {
